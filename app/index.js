500a565198afc0309b5b3b3c121d6d147abdd3b4
Brian Balberchak <b.balberchak@outlook.com>
2024-01-23 22:14:52 -0800

    Initial implementation of the Heart Rate monitor.

--- app/index.js
+++ app/index.js
@@ -1,17 +1,68 @@
 import clock from "clock";
 import * as document from "document";
-import { me } from "appbit";
+import { me as appbit } from "appbit";
 import { preferences } from "user-settings";
 import { today, goals } from "user-activity";
 import { battery } from "power";
 import { charger } from "power";
+import { display } from "display";
+import { HeartRateSensor } from "heart-rate";
+import { BodyPresenceSensor } from "body-presence";
 
 const BATTERY_LVL_NORMAL    = 50;
 const BATTERY_LVL_LOW       = 25;
 
-if (me.permissions.granted("access_activity")) 
+// Get a handle for each element
+const clock_time        = document.getElementById("clock_time");
+const step_icon         = document.getElementById("step_icon");
+const step_count        = document.getElementById("step_count");
+const batt_icon         = document.getElementById("batt_icon");
+const batt_bar          = document.getElementById("batt_bar");
+const batt_bar_shine    = document.getElementById("batt_bar_shine");
+const heart_rate        = document.getElementById("heart_rate");
+
+const hrm               = new HeartRateSensor({ frequency: 1});
+
+// Check permissions
+if (appbit.permissions.granted("access_activity")) 
 {
     console.log(`${today.adjusted.steps} Steps`);
+}
+
+if (HeartRateSensor && appbit.permissions.granted("access_heart_rate")) 
+{
+    hrm.addEventListener("reading", () => 
+    {
+        heart_rate.text = `${hrm.heartRate}`;
+    });
+
+    display.addEventListener("change", () => 
+    {
+        // Automatically stop the sensor when the screen is off to conserve battery
+        display.on ? hrm.start() : hrm.stop();
+    });
+
+    hrm.start();
+}
+
+if (BodyPresenceSensor) 
+{
+    const body = new BodyPresenceSensor();
+
+    body.addEventListener("reading", () => 
+    {
+        if (!body.present) 
+        {
+            hrm.stop();
+            heart_rate.text = '--';
+        } 
+        else 
+        {
+            hrm.start();
+        }
+    });
+
+    body.start();
 }
 
 function zeroPad(i) 
@@ -23,23 +74,14 @@ function zeroPad(i)
     return i;
 }
 
-// Update the clock every minute
+// Update the clock every second
 clock.granularity = "seconds";
-
-// Get a handle on the <text> element
-const myLabel           = document.getElementById("myLabel");
-const step_icon         = document.getElementById("step_icon");
-const step_count        = document.getElementById("step_count");
-const batt_icon         = document.getElementById("batt_icon");
-const batt_bar          = document.getElementById("batt_bar");
-const batt_bar_shine    = document.getElementById("batt_bar_shine");
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => 
 {
     let today = evt.date;
     let hours = today.getHours();
-    // let flicker = true;
 
     if (preferences.clockDisplay === "12h") 
     {
@@ -54,7 +96,7 @@ clock.ontick = (evt) =>
     let mins = zeroPad(today.getMinutes());
 
     // Display Time
-    myLabel.text = `${hours}:${mins}`;
+    clock_time.text = `${hours}:${mins}`;
 
     // Update Battery Bar
     battery.onchange(charger, evt);
